4a1a8be4d20428d2b0d4263cd6b967f8d225de7f
Cody Moss <codytyler.moss@gmail.com>
2014-07-27 19:20:36 -0600

    more debugging

--- app.js
+++ app.js
@@ -7,27 +7,27 @@
       $stateProvider
         .state('kimchi', {
             url: "/kimchi",
-            templateUrl: "kimchi.html"
+            templateUrl: "route1.html"
         })
-          .state('kimchi.more', {
-              url: "/more",
-              templateUrl: "kimchi.more.html",
-              // controller: function($scope){
-              //   $scope.items = ["A", "List", "Of", "Items"];
-              // }
+          .state('route1.list', {
+              url: "/list",
+              templateUrl: "route1.list.html",
+              controller: function($scope){
+                $scope.items = ["A", "List", "Of", "Items"];
+              }
           })
           
-        .state('kalbi', {
-            url: "/kalbi",
-            templateUrl: "kalbi.html"
+        .state('route2', {
+            url: "/route2",
+            templateUrl: "route2.html"
         })
-          .state('kalbi.more', {
-              url: "/more",
-              templateUrl: "kalbi.more.html",
-          //     controller: function($scope){
-          //       $scope.things = ["A", "Set", "Of", "Things"];
-          //     }
-          // })
+          .state('route2.list', {
+              url: "/list",
+              templateUrl: "route2.list.html",
+              controller: function($scope){
+                $scope.things = ["A", "Set", "Of", "Things"];
+              }
+          })
 
           .state('home', {
               url: "/home",
@@ -38,3 +38,46 @@
 
 
     })
+
+
+
+ // var yumShik = angular.module('yumShik', ["ui.router"])
+ //    yumShik.config(function($stateProvider, $urlRouterProvider){
+      
+ //      // For any unmatched url, send to /route1
+ //      $urlRouterProvider.otherwise("/home")
+      
+ //      $stateProvider
+ //        .state('kimchi', {
+ //            url: "/kimchi",
+ //            templateUrl: "kimchi.html"
+ //        })
+ //          .state('kimchi.more', {
+ //              url: "/more",
+ //              templateUrl: "kimchi.more.html",
+ //              // controller: function($scope){
+ //              //   $scope.items = ["A", "List", "Of", "Items"];
+ //              // }
+ //          })
+          
+ //        .state('kalbi', {
+ //            url: "/kalbi",
+ //            templateUrl: "kalbi.html"
+ //        })
+ //          .state('kalbi.more', {
+ //              url: "/more",
+ //              templateUrl: "kalbi.more.html",
+ //          //     controller: function($scope){
+ //          //       $scope.things = ["A", "Set", "Of", "Things"];
+ //          //     }
+ //          // })
+
+ //          .state('home', {
+ //              url: "/home",
+ //              templateUrl: "home.html"
+ //          })
+
+
+
+
+ //    })
