7c2fb4dc00ce4b04f7a097fe825f1038a4f17f05
Cody Moss <codytyler.moss@gmail.com>
2014-07-27 19:35:03 -0600

    jjampong

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
       $stateProvider
         .state('kimchi', {
             url: "/kimchi",
-            templateUrl: "route1.html"
+            templateUrl: "kimchi.html"
         })
           .state('kimchi.more', {
               url: "/more",
@@ -28,6 +28,31 @@
               //   $scope.things = ["A", "Set", "Of", "Things"];
               // }
           })
+
+            .state('jjamppong', {
+            url: "/jjamppong",
+            templateUrl: "jjamppong.html"
+        })
+          .state('jjamppong.more', {
+              url: "/more",
+              templateUrl: "jjamppong.more.html",
+              // controller: function($scope){
+              //   $scope.items = ["A", "List", "Of", "Items"];
+              // }
+          })
+
+            .state('jjajangmeon', {
+            url: "/jjajangmeon",
+            templateUrl: "jjajangmeon.html"
+        })
+          .state('jjajangmeon.more', {
+              url: "/more",
+              templateUrl: "jjajangmeon.more.html",
+              // controller: function($scope){
+              //   $scope.items = ["A", "List", "Of", "Items"];
+              // }
+          })
+
 
           .state('home', {
               url: "/home",
