1313c48b8d2652e908474d08f754831066317585
Cody Moss <codytyler.moss@gmail.com>
2014-07-27 18:28:02 -0600

    getting the start to yumshik

---
+++ app.js
@@ -0,0 +1,40 @@
+ var yumShik = angular.module('yumShik', ["ui.router"])
+    yumShik.config(function($stateProvider, $urlRouterProvider){
+      
+      // For any unmatched url, send to /route1
+      $urlRouterProvider.otherwise("/home")
+      
+      $stateProvider
+        .state('kimchi', {
+            url: "/kimchi",
+            templateUrl: "route1.html"
+        })
+          .state('route1.list', {
+              url: "/list",
+              templateUrl: "route1.list.html",
+              controller: function($scope){
+                $scope.items = ["A", "List", "Of", "Items"];
+              }
+          })
+          
+        .state('route2', {
+            url: "/route2",
+            templateUrl: "route2.html"
+        })
+          .state('route2.list', {
+              url: "/list",
+              templateUrl: "route2.list.html",
+              controller: function($scope){
+                $scope.things = ["A", "Set", "Of", "Things"];
+              }
+          })
+
+          .state('home', {
+              url: "/home",
+              templateUrl: "home.html"
+          })
+
+
+
+
+    })
